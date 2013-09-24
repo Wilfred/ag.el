654492cb73d1693b1ccb490e10ee05b455dcfdc2
Wilfred Hughes <me@wilfred.me.uk>
2013-09-24 14:53:14 +0100

    just point to the readme for documentation

--- ag.el
+++ ag.el
@@ -8,26 +8,8 @@
 
 ;;; Commentary:
 
-;; This file is heavily based on the excellent ack-and-a-half.el.
-
-;; Usage:
-
-;; Add to your .emacs.d:
-
-;; (add-to-list 'load-path "/path/to/ag.el") ;; optional
-;; (require 'ag)
-
-;; Alternatively, just install the package from MELPA.
-
-;; If you're using ag 0.14+, which supports --color-match, then you
-;; can add highlighting with:
-
-;; (setq ag-highlight-search t)
-
-;; I like to bind the *-at-point commands to F5 and F6:
-
-;; (global-set-key (kbd "<f5>") 'ag-project)
-;; (global-set-key (kbd "<f6>") 'ag-regexp-project-at-point)
+;; Please see README.md for documentation, or read in online at
+;; https://github.com/Wilfred/ag.el/#agel
 
 ;;; License:
 
