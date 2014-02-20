f811b7f38396d83c6b95376ded4e06aecef525b7
Wilfred Hughes <me@wilfred.me.uk>
2014-02-20 23:07:35 +0000

    Writing a macro for mocking functions.

--- tests.el
+++ tests.el
@@ -1,4 +1,16 @@
-(require 'mocker)
+(defmacro with-mock-func (func-name mock &rest body)
+  "Evaluate BODY with the function FUNC-NAME (a symbol) bound to MOCK.
+Returns the last value in BODY, same as `progn'."
+  `(let ((old-func-value (symbol-function ',func-name)))
+    (prog2
+        (fset ',func-name ,mock)
+        (progn ,@body)
+      (fset ',func-name old-func-value))))
+
+(defun add-one (x) (1+ x))
+
+(with-mock-func add-one (lambda (x) (* x 2))
+                (add-one 5))
 
 (mocker-let
     ((compilation-start
