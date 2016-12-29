0b4f5b5678479aa0593dbfae06a96aada5016094
Wilfred Hughes <me@wilfred.me.uk>
2016-12-29 00:37:34 +0000

    Fix failing tests

--- test/unit-test.el
+++ test/unit-test.el
@@ -2,16 +2,17 @@
 (require 'ag)
 
 (ert-deftest ag--escape-pcre ()
-  (should (equal (ag--escape-pcre "ab.*(") "ab\\.\\*\\(")))
+  (should (equal (ag/escape-pcre "ab.*(") "ab\\.\\*\\(")))
 
 (ert-deftest ag--longest-string ()
   (should
-   (equal (ag--longest-string nil "foo" nil "f" "foobarbaz" "z")
+   (equal (ag/longest-string nil "foo" nil "f" "foobarbaz" "z")
           "foobarbaz")))
 
 (ert-deftest ag-project ()
   "Smoke test for what is probably the most useful command in ag.el"
-  (ag-project "defun")
+  (switch-to-buffer (ag-project "defun"))
   (should
    (equal (buffer-name)
-          (format "*ag search text:defun dir:%s*" default-directory))))
+          (format "*ag search text:defun dir:%s*"
+                  (f-abbrev default-directory)))))
